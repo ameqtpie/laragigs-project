d0cb4062d1a66de43ae8b8a292419e1abcfba452
ameqtpie <rainierguinsatao09@gmail.com>
2023-05-01 20:56:22 +0800

    5/1/23 - update

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,12 @@ use Illuminate\Support\Facades\Route;
 // all listing
 Route::get('/', [ListingController::class, 'index']);
 
+// show create form 
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// store listing data
+Route::post('/listings', [ListingController::class, 'store']);
+
 // single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
